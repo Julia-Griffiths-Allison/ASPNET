b6786aeb81f8ace2c39667511d2b0a7dd76049ef
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-10-25 17:16:06 -0400

    created an 'update thisd product' button on the individual product pages

--- Testing/IProductRepo.cs
+++ Testing/IProductRepo.cs
@@ -6,5 +6,6 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
     }
 }
